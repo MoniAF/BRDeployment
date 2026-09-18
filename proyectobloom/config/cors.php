99c7d8682df483c5ceed406165f67f00e5797437
Monica Artavia Flores <artaviafloresmonica@gmail.com>
2026-09-17 18:24:41 -0600

    Configure Laravel Docker deployment and frontend CORS

--- proyectobloom/config/cors.php
+++ proyectobloom/config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => array_filter([
+        env('FRONTEND_URL'),
+    ]),
 
     'allowed_origins_patterns' => [],
 
