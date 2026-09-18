8e0af00c80d894593d925c3ab2fd5a653fc251d0
Monica Artavia Flores <artaviafloresmonica@gmail.com>
2026-09-17 19:30:56 -0600

    Update API connection

--- bloomrecipes/src/services/api.js
+++ bloomrecipes/src/services/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
-export const BACKEND_URL = 'http://127.0.0.1:8000'
+export const BACKEND_URL =
+  import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api'
+
 export const AUTH_TOKEN_KEY = 'bloom_token'
 
 const api = axios.create({
-  baseURL: `${BACKEND_URL}/api/`,
+  baseURL: `${BACKEND_URL}/`,
   timeout: 15000,
   headers: {
     Accept: 'application/json',
